f1a468512aa8c38bd1b0380b5d92351bcd83e250
Ilemobayo Samson <sam.ilemobayo@gmail.com>
2023-10-05 00:34:53 +0100

    Samson

---
+++ src/main/java/com/paygo/paygo/dto/CustomerDto.java
@@ -0,0 +1,34 @@
+package com.paygo.paygo.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String mnemonic;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String middleName;
+
+    private String maidenName;
+
+    private String address;
+
+    private String country;
+
+    private String state;
+
+    private String email;
+
+    private String idType;
+
+    private String idNo;
+
+    private String mobileNo;
+    
+    private String altMobileNo;
+}
